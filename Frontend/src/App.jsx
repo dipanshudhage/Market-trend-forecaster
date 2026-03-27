3ffb5e7f21c6ebe7bcb4bfcb2eabedd5a53e4180
DIPANSHU DHAGE <156861498+dipanshudhage@users.noreply.github.com>
2026-03-27 20:06:53 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AIChatbot from "../pages/AIChatbot";
 
-function App() {
+function AppRoutes() {
   return (
-    <Router>
-      <AppRoutes />
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/chatbot" element={<AIChatbot />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default AppRoutes;
