522aabee66cb3b6bd79afb283cceee79d6619a3e
DIPANSHU DHAGE <156861498+dipanshudhage@users.noreply.github.com>
2026-03-27 20:14:58 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AIChatbot from "./pages/AIChatbot";
 
-function AppRoutes() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<h1>Home</h1>} />
         <Route path="/chatbot" element={<AIChatbot />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default AppRoutes;
+export default App;
